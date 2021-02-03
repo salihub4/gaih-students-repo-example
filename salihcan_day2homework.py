81c19b826d46122d8c7d70c807da6baf7f4c83ec
salihub4 <77972896+salihub4@users.noreply.github.com>
2021-02-03 16:05:50 +0300

    Add files via upload

---
+++ salihcan_day2homework.py
@@ -0,0 +1,20 @@
+#1
+list1 = [1, 2, 3, 4, 5]
+list2 = [6, 7, 8, 9, 10]
+
+list1, list2 = list2, list1
+
+print("list1 value :", list1)
+print("list2 value :", list2)
+
+s = input("Can you enter a single digit integer?")
+
+#2
+try:
+    if int(s)>=0:
+        print("Even Numbers :")
+        for i in range(int(s)):
+            if i % 2 == 0:
+                print(i)
+except:
+        print("Llease enter number.")
